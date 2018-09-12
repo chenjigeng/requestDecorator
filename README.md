a1980a71b20cbdd76edff2a8aaab5d0fb49e8063
chenjigeng <178854407@qq.com>
2018-09-13 01:06:13 +0800

    feat:修改readme

---
+++ README.md
@@ -0,0 +1,25 @@
+## RequestDecorator
+
+这是一个允许你对你的request api做并发限制的一个小工具。
+
+使用样例：
+
+```javascript
+// 一个callback类型的请求api
+async function delay(num, time, cb) {
+  setTimeout(() => {
+    cb(null, num);
+  }, time);
+}
+
+// 通过maxLimit设置并发量限制，needChange2Promise将callback类型的请求api转化为promise类型的。
+const requestInstance = new RequestDecorator({
+  maxLimit: 5,
+  requestApi: delay,
+  needChange2Promise: true,
+});
+
+// 接下来你就可以像原来使用你的api那样使用它,参数和原来的是一样的
+requestInstance.request(10, Math.random() * 3000)
+  .then(result => console.log('result', result), error => console.log(error))
+```
--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 const RequestDecorator = require('../src/index.js')
 
+// 一个callback类型的请求api
 async function delay(num, time, cb) {
   setTimeout(() => {
     cb(null, num);
   }, time);
 }
+
+// 通过maxLimit设置并发量限制，needChange2Promise将callback类型的请求api转化为promise类型的。
 const requestInstance = new RequestDecorator({
   maxLimit: 5,
   requestApi: delay,
@@ -14,6 +17,7 @@ const requestInstance = new RequestDecorator({
 
 let promises = [];
 for (let i = 0; i < 30; i++) {
+  // 接下来你就可以像原来使用你的api那样使用它,参数和原来的是一样的
   promises.push(requestInstance.request(i, Math.random() * 3000).then(result => console.log('result', result), error => console.log(error)));
 }
 async function test() {
